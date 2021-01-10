596d9975a637ddb679a9da4dd2b988701208ea5c
Vishal <59360979+69Vishal@users.noreply.github.com>
2021-01-10 22:34:55 +0530

    added time

--- data-log.py
+++ data-log.py
@@ -1,35 +1,36 @@
 import serial
 import csv, time
 try:
-    ser = serial.Serial("COM4", 115200) # change your respective serial port with COM4 and serial rate with 115200.
+    ser = serial.Serial("COM4", 115200) # change your serial port with COM4
     serial_try = True
 except:
-    # if no serial communicating device is detected then program will show error for 2 seconds.
+    # gives failed to connect to serial port error if serial port is not available
     serial_try = False
     print('Failed to connect to serial port')
     time.sleep(2)
     exit()
-# create a file name data.csv
+# create a file data.csv for storing sensor values
 f2 = open('data.csv', 'w')
 writer = csv.writer(f2)
-writer.writerow(['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y','gyro_z','temperature']) 
+writer.writerow(['time (ms)','acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y','gyro_z','temperature']) 
 
 while serial_try:
-    # this loop will look for seial data and will save it to csv file
     try:
+        # processes raw values from arduino and store it in data.csv which we created earlier
         raw_data = str(ser.readline())
         data = raw_data[2:-5]
         split_data = data.split(',')
-        result = time.localtime(time.time())
-        acc_x = split_data[0]
-        acc_y = split_data[1]
-        acc_z = split_data[2]
-        gyro_x = split_data[3][1:]
-        gyro_y = split_data[4]
-        gyro_z = split_data[5]
-        temperature = split_data[6]
-        writer.writerow([acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature]) 
-        print(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature)
+        time = split_data[0]
+        acc_x = split_data[1]
+        acc_y = split_data[2]
+        acc_z = split_data[3]
+        gyro_x = split_data[4][1:]
+        gyro_y = split_data[5]
+        gyro_z = split_data[6]
+        temperature = split_data[7]
+        writer.writerow([time,acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature]) 
+        print(time, acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, temperature)
 
     except:
+        # if any error is found the system will show error!
         print('Error!')
